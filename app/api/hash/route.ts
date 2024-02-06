6a3b9656ee00604d5dd5e03701cd5c989613b2d4
libdefi <libcreatesweb3@gmail.com>
2024-02-06 13:38:05 +0800

    Recast and Like reconfiguration

--- app/api/hash/route.ts
+++ app/api/hash/route.ts
@@ -3,11 +3,11 @@ import { NeynarAPIClient, CastParamType } from "@neynar/nodejs-sdk";
 
 export async function GET() {
   const client = new NeynarAPIClient(process.env.NEYNAR_API_KEY || '');
-  const url = "https://warpcast.com/baratie/0xdcb08b3a"; // Change this to your warpcast post url
+  const url = "https://warpcast.com/baratie/0xa08b5c5a";
   const cast = await client.lookUpCastByHashOrWarpcastUrl(url, CastParamType.Url);
   console.log(cast);
   const jsonCast = JSON.stringify(cast);
   return new NextResponse(jsonCast);
 }
 
-// 0xdcb08b3ad3f9e347dac5a9b1dc4336d2f720a1d3
+// 0xa08b5c5a8e753d2ea3af1421d0f1d841a3e5a119
