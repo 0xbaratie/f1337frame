2dbcd64f63a66af0f641eaa57726ae32d62a411a
libdefi <libcreatesweb3@gmail.com>
2024-02-06 08:13:26 +0800

    Update route.ts

--- app/api/hash/route.ts
+++ app/api/hash/route.ts
@@ -3,9 +3,11 @@ import { NeynarAPIClient, CastParamType } from "@neynar/nodejs-sdk";
 
 export async function GET() {
   const client = new NeynarAPIClient(process.env.NEYNAR_API_KEY || '');
-  const url = "https://warpcast.com/baratie/0x508754e9"; // Change this to your warpcast post url
+  const url = "https://warpcast.com/baratie/0xdcb08b3a"; // Change this to your warpcast post url
   const cast = await client.lookUpCastByHashOrWarpcastUrl(url, CastParamType.Url);
   console.log(cast);
   const jsonCast = JSON.stringify(cast);
   return new NextResponse(jsonCast);
 }
+
+// 0xdcb08b3ad3f9e347dac5a9b1dc4336d2f720a1d3
